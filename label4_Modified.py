f80b3ce04992c6838b86524a3be3e23cc1a3b2b5
Temi Oketunji <toketunji@gmail.com>
2017-05-30 15:19:30 -0400

    Temi

---
+++ label4_Modified.py
@@ -0,0 +1,23 @@
+#!/bin/python
+# script written by Olu for ags_technology training
+# all rights reserved
+# 11/05/2017
+
+import sys,datetime
+
+def cal_interest (p,t,r):
+        return p*t*r/100
+
+def write_to_disk (data):
+        text_file = open("data.txt", "a")
+        time_stamp = datetime.datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
+        text_file.write("\nTimestamp: %s \t  %s" % (time_stamp, data))
+        text_file.close()
+
+if __name__ == '__main__':
+        interest = cal_interest (float(sys.argv[1]), int(sys.argv[2]), float(sys.argv[3]))
+        print "Interest is calculated as %0.2f percent " %interest
+        info = 'P = ' + sys.argv[1] +  ' T = ' + sys.argv[1] + ' R = ' + sys.argv[3]
+        write_to_disk (info)
+
+
